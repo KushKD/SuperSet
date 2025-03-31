ecc0db85c8c9af4fe9481a00dbd8ef8d09ce67d3
Kush Dhawan <kushkumardhawan@gmail.com>
2025-03-31 17:27:31 +0530

    cxv

---
+++ src/notes/config/run_superset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export SUPERSET_CONFIG_PATH=/superset_app/superset_test1/superset_config.py
+ . /superset_app/superset_test1/superset_env/bin/activate
+gunicorn \
+      -w 10 \
+      -k gevent \
+      --timeout 120 \
+      -b  0.0.0.0:8088 \
+      --limit-request-line 0 \
+      --limit-request-field_size 0 \
+      --statsd-host localhost:8125 \
+      "superset.app:create_app()"
